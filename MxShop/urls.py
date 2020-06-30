cc70c21f8239adb2c817c3f9ddf171c770ee4ec1
zengmingjie <zengmingjie@yy.com>
2020-06-30 20:37:09 +0800

    update

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 import xadmin
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
-    path(r'^xadmin/', xadmin.site.urls),
+    url(r'^xadmin/', xadmin.site.urls),
 ]
